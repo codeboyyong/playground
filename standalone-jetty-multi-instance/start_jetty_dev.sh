0c9b16a8f75f918aa42d450b6c4530fb2b3d3eed
John Zhao <codeboyyong@gmail.com>
2014-06-18 16:51:19 -0700

    remove the .d folder

--- standalone-jetty-multi-instance/start_jetty_dev.sh
+++ standalone-jetty-multi-instance/start_jetty_dev.sh
@@ -1,1 +1,1 @@
-java -jar start.jar OPTIONS=Server,jsp --ini=webapps/dev/start.d/dev.ini webapps/dev/start.d/jetty_dev.xml
+java -jar start.jar OPTIONS=Server,jsp --ini=webapps/dev/start.d/dev.ini webapps/dev/jetty_dev.xml
--- standalone-jetty-multi-instance/start_jetty_stage.sh
+++ standalone-jetty-multi-instance/start_jetty_stage.sh
@@ -1,1 +1,1 @@
-java -jar start.jar OPTIONS=Server,jsp --ini=webapps/dev/start.d/stage.ini webapps/dev/start.d/jetty_stage.xml
+java -jar start.jar OPTIONS=Server,jsp --ini=webapps/dev/start.d/stage.ini webapps/dev/jetty_stage.xml
